9df4576187fbe92e2f618b20f9c4ba8d9d5c720f
Phillip Maree <phillip.maree@gmail.com>
2024-10-01 13:33:55 +0200

    Yet more improvements in verbose

--- lib/ml/ml.py
+++ lib/ml/ml.py
@@ -7,6 +7,7 @@ from typing import Tuple
 from torch import nn
 import polars as pl
 import numpy as np
+import time
 
 from lib import logger
 from lib.ml.dataloader import DataLoader
@@ -199,15 +200,20 @@ class Ml:
 
     def train(self):
 
+        train_idx = 0
+        val_idx = 0
+
         epoch_ave_vloss = np.nan
         epoch_ave_vacc = np.nan
 
         for epoch_i in range(self.params['num_epochs']):
-
+            t0_epoch = time.time()
             self.model.train()
 
             epoch_ave_loss = 0
             epoch_ave_acc = 0
+
+            t0 = time.time()
 
             for train_i, train_data in enumerate(self.train_loader):
 
@@ -231,10 +237,14 @@ class Ml:
                 epoch_ave_loss = (loss.item() + train_i*epoch_ave_loss)/(train_i+1)
                 epoch_ave_acc = (acc.item() + train_i*epoch_ave_acc)/(train_i+1)
 
-                self.writer.add_scalar('Loss/train', loss.item())
-                self.writer.add_scalar('Acc/train', acc.item())
+                # TODO some verbose / logging to be improved
+                self.writer.add_scalar('Loss/train', loss.item(), train_idx)
+                self.writer.add_scalar('Acc/train', acc.item(), train_idx)
+                train_idx +=1
 
-                logger.info(f"\033[F\rEPOCH {epoch_i+1}/{self.params['num_epochs']} TRAIN {(train_i+1)/self.train_loader.iter_cnt*100:.0f}%: loss={epoch_ave_loss:.4f} - acc={epoch_ave_acc:.4f} VAL {0}%: val_loss={epoch_ave_vloss:.4f} - val_acc={epoch_ave_vacc:.4f}",end='', color=logger.BLUE)
+                train_it_per_sec = train_i/(time.time()-t0)
+                logger.info(f"\033[F\rEPOCH {(epoch_i+1)/self.params['num_epochs']*100:.0f}% ({epoch_i+1}/{self.params['num_epochs']}) [{time.time() - t0_epoch:.3f} sec]|TRAIN {(train_i+1)/self.train_loader.iter_cnt*100:.0f}% ({train_i+1}/{self.train_loader.iter_cnt}): loss={epoch_ave_loss:.4f} - acc={epoch_ave_acc:.4f} [{train_it_per_sec:.4f} it/sec]|VAL {0}%: val_loss={epoch_ave_vloss:.4f} - val_acc={epoch_ave_vacc:.4f} [- it/sec]",end='', color=logger.BLUE)
+                # TODO end
 
             self.model.eval()
 
@@ -242,6 +252,7 @@ class Ml:
             epoch_ave_vacc = 0
 
             with torch.no_grad():
+                t0 = time.time()
                 for val_i, val_data in enumerate(self.val_loader):
                     (_, inputs, inputs_exo, targets) = val_data.values()
 
@@ -254,15 +265,20 @@ class Ml:
                     epoch_ave_vloss = (loss.item() + val_i*epoch_ave_vloss)/(val_i+1)
                     epoch_ave_vacc = (acc.item() + val_i*epoch_ave_vacc)/(val_i+1)
 
-                    self.writer.add_scalar('Loss/val', loss.item())
-                    self.writer.add_scalar('Acc/val', acc.item())
+                    # TODO some verbose / logging to be improved
+                    self.writer.add_scalar('Loss/val', loss.item(), val_idx)
+                    self.writer.add_scalar('Acc/val', acc.item(), val_idx)
+                    val_idx +=1
 
-                    logger.info(f"\033[F\rEPOCH {epoch_i+1}/{self.params['num_epochs']} TRAIN {(train_i+1)/self.train_loader.iter_cnt*100:.0f}%: loss={epoch_ave_loss:.4f} - acc={epoch_ave_acc:.4f} VAL {(val_i+1)/self.val_loader.iter_cnt*100:.0f}%: val_loss={epoch_ave_vloss:.4f} - val_acc={epoch_ave_vacc:.4f}",end='', color=logger.BLUE)
+                    val_it_per_sec = val_i/(time.time()-t0)
+                    logger.info(f"\033[F\rEPOCH {(epoch_i+1)/self.params['num_epochs']*100:.0f}% ({epoch_i+1}/{self.params['num_epochs']}) [{time.time() - t0_epoch:.3f} sec]|TRAIN {(train_i+1)/self.train_loader.iter_cnt*100:.0f}% ({train_i+1}/{self.train_loader.iter_cnt}): loss={epoch_ave_loss:.4f} - acc={epoch_ave_acc:.4f} [{train_it_per_sec:.4f} it/sec]|VAL {(val_i+1)/self.val_loader.iter_cnt*100:.0f}% ({val_i+1}/{self.val_loader.iter_cnt}): val_loss={epoch_ave_vloss:.4f} - val_acc={epoch_ave_vacc:.4f} [{val_it_per_sec:.4f} it/sec]",end='', color=logger.BLUE)
 
                 logger.info('')
 
+
             self.writer.add_scalars('Loss/epoch', {'train': epoch_ave_loss, 'val': epoch_ave_vloss}, epoch_i)
             self.writer.add_scalars('Acc/epoch', {'train': epoch_ave_acc, 'val': epoch_ave_vacc}, epoch_i)
+            # TODO end
 
         model_path = os.path.join(self.artifacts_path, f'__{self.model.name.lower()}__.pytorch')
         torch.save(self.model.state_dict(), model_path)
